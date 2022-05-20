524879b3caa198721b3971c76169c9d7e3668200
Misato Tanno <breitmeer.0804@gmail.com>
2022-05-19 23:07:22 -0700

    Added js function

--- public/js/script.js
+++ public/js/script.js
@@ -1,25 +1,42 @@
 
 let countryList = document.getElementById("country_List")
-// console.log(countryList);
 let country = countryList.children;
 let countryArray = Array.from(country);
-// console.log(countryArray);
 let selectRegion = document.getElementById("country-select");
-// console.log(selectRegion);
-console.log(countryArray[0].classList[1] == "Asia")
 
-    // console.log(countryArray[0].className.contains('asia'))
-
-function selectRegion() {
+function selectoRegion() {
     selectRegion.addEventListener('change', function () {
         var selectedRegion = selectRegion.value;
         for (let i = 0; i < countryArray.length; i++) {
             if (countryArray[i].classList[1] !== selectedRegion) {
                 countryArray[i].style.display = "none";
+            } else {
+                countryArray[i].style.display = "block";
             }
         }
     })
 }
+
+selectoRegion();
+
+let user_input = document.getElementById("user_input");
+console.log(countryArray[1].className);
+
+function checkString() {
+    user_input.addEventListener('keyup', function() {
+        let value = user_input_value = user_input.value;
+        for (let i = 0; i < countryArray.length; i++) {
+            if(countryArray[i].classList[2].includes(value)) {
+                countryArray[i].style.display = "block";
+            } else {
+                countryArray[i].style.display = "none";
+            }
+        }
+    })    
+}
+
+checkString();
+
 
 
 
